4534c3a56108a1f1c047edf31bc4d65aef454a2f
dmosyan <dmosyan@gmail.com>
2023-03-02 22:41:44 -0500

    return child resource of city

---
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -0,0 +1,21 @@
+﻿using CityInfo.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/cities/{cityId}/pointsofinterest")]
+    [ApiController]
+    public class PointsOfInterestController : ControllerBase
+    {
+        public ActionResult<IEnumerable<PointOfInterestDto>> GetPointsOfInterest(int cityId)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+            return Ok(city.PointsOfInterest);
+        }
+    }
+}
