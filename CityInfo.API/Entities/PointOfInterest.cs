a2111520d75ede4e8c57762b31e9285759fb463a
dmosyan <dmosyan@gmail.com>
2023-03-13 16:58:54 -0400

    add name required and description max length

--- CityInfo.API/Entities/PointOfInterest.cs
+++ CityInfo.API/Entities/PointOfInterest.cs
@@ -8,6 +8,9 @@ namespace CityInfo.API.Entities
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string Name { get; set; }
 
         [ForeignKey("CityId")]
