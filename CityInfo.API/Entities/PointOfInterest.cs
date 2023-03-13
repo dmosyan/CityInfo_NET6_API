8592e83549fba1630552f701d909e50c6f7020ab
dmosyan <dmosyan@gmail.com>
2023-03-13 16:56:24 -0400

    add navigation property in point of interest

--- CityInfo.API/Entities/PointOfInterest.cs
+++ CityInfo.API/Entities/PointOfInterest.cs
@@ -10,6 +10,10 @@ namespace CityInfo.API.Entities
         public int Id { get; set; }
         public string Name { get; set; }
 
+        [ForeignKey("CityId")]
+        public City? City { get; set; }
+        public int CityId { get; set; }
+
         public PointOfInterest(string name)
         {
             Name = name;
