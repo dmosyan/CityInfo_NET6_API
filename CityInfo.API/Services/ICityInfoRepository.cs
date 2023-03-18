96c7486b6f294f11c4b14403f3e8027a4b2dba26
dmosyan <dmosyan@gmail.com>
2023-03-18 12:31:43 -0400

    add repository interface

---
+++ CityInfo.API/Services/ICityInfoRepository.cs
@@ -0,0 +1,15 @@
+﻿using CityInfo.API.Entities;
+
+namespace CityInfo.API.Services
+{
+    public interface ICityInfoRepository
+    {
+        Task<IEnumerable<City>> GetCitiesAsync();
+
+        Task<City?> GetCity(int cityId);
+
+        Task<IEnumerable<PointOfInterest>> GetPointsOfInterestForCityAsync(int cityId);
+
+        Task<PointOfInterest?> GetPointOfInterestForCityAsync(int cityId, int pointOfInterestId);
+    }
+}
