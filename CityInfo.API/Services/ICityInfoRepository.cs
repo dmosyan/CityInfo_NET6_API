efc853a79407f16a618eb8dd294f1e9b65b7c92f
dmosyan <dmosyan@gmail.com>
2023-03-18 12:44:42 -0400

    implement repository interface

--- CityInfo.API/Services/ICityInfoRepository.cs
+++ CityInfo.API/Services/ICityInfoRepository.cs
@@ -8,7 +8,7 @@ namespace CityInfo.API.Services
 
         Task<City?> GetCity(int cityId);
 
-        Task<IEnumerable<PointOfInterest>> GetPointsOfInterestForCityAsync(int cityId);
+        Task<IEnumerable<PointOfInterest>> GetPointsOfInterestForCityAsync(int cityId, bool includePointsOfInterest);
 
         Task<PointOfInterest?> GetPointOfInterestForCityAsync(int cityId, int pointOfInterestId);
     }
