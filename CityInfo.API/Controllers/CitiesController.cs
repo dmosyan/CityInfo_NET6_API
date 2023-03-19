d242432909985ad6376505d621ec953927e7af92
dmosyan <dmosyan@gmail.com>
2023-03-19 16:55:56 -0400

    add new dto and replace in memory data with db data

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -1,4 +1,5 @@
 ﻿using CityInfo.API.Models;
+using CityInfo.API.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CityInfo.API.Controllers
@@ -7,33 +8,50 @@ namespace CityInfo.API.Controllers
     [Route("api/cities")]
     public class CitiesController : ControllerBase
     {
-        private readonly CitiesDataStore _citiesDataStore;
+        private readonly ICityInfoRepository _cityInfoRepository;
 
-        public CitiesController(CitiesDataStore citiesDataStore)
+        public CitiesController(ICityInfoRepository cityInfoRepository)
         {
-            _citiesDataStore = citiesDataStore?? throw new ArgumentNullException(nameof(citiesDataStore));
+            _cityInfoRepository = cityInfoRepository?? throw new ArgumentNullException(nameof(cityInfoRepository));
         }
 
 
         [HttpGet()]
-        public ActionResult<IEnumerable<CityDto>> GetCities()
+        public async Task<ActionResult<IEnumerable<CityWithoutPointsOfInterestDto>>> GetCities()
         {
-            var citiesToReturn = _citiesDataStore.Cities;
-            return Ok(citiesToReturn);
+            //var citiesToReturn = _citiesDataStore.Cities;
+            //return Ok(citiesToReturn);
+
+            var cityEntites = await _cityInfoRepository.GetCitiesAsync();
+
+            var results = new List<CityWithoutPointsOfInterestDto>();
+            foreach (var cityEntity in cityEntites)
+            {
+                results.Add(new CityWithoutPointsOfInterestDto
+                {
+                    Id = cityEntity.Id,
+                    Name = cityEntity.Name,
+                    Description = cityEntity.Description
+                });
+            }
+
+            return Ok(results);
         }
 
         [HttpGet("{id}")]
         public ActionResult<CityDto> GetCity(int id)
         {
-            var cityToReturn = _citiesDataStore.Cities
-                .FirstOrDefault(c => c.Id == id);
+            //var cityToReturn = _citiesDataStore.Cities
+            //    .FirstOrDefault(c => c.Id == id);
 
-            if (cityToReturn == null)
-            {
-                return NotFound();
-            }
+            //if (cityToReturn == null)
+            //{
+            //    return NotFound();
+            //}
 
-            return Ok(cityToReturn);
+            //return Ok(cityToReturn);
+
+            return Ok();
         }
     }
 }
